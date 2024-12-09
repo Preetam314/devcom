c8b2ce53e9402e1ba42e24d0efa60f47654cc49c
Preetam314 <preetamjutanatti06@gmail.com>
2024-12-09 19:07:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+/*
+const navbar = document.getElementById("navbar"); //for getting navbar element
+window.onscroll = function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        navbar.classList.add("shrink");}
+        else{
+            navbar.classList.remove("shrink");
+        }
+    }
+
+*/
